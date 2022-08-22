6554f5969f4843eb18a5f7666d28fd0c09bf2de5
saralemon <sara1.benedetti@gmail.com>
2022-08-22 11:04:01 +0200

    Initial commit

---
+++ src/test/java/it/uniroma3/siw/springcertificazioni/SpringCertificazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.springcertificazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCertificazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
